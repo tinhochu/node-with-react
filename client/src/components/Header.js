32f70bb75429f6e44c46334dfb4d3aacfcca7ac2
Tin Chu <mchu.tinho@gmail.com>
2018-03-19 18:14:24 -0500

    billing and middleware

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -16,10 +16,13 @@ class Header extends Component {
 				);
 			default:
 				return [
-					<li key={1}>
+					<li key={2}>
 						<Payments />
 					</li>,
-					<li key={2}>
+					<li key={1} style={{ margin: '0 10px' }}>
+						Credits: {this.props.auth.credits}
+					</li>,
+					<li key={3}>
 						<a href="/api/logout">Logout</a>
 					</li>
 				];
